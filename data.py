b5947c2a20bc38219cf7fcd9887eef5e455c710c
Nicci Tynen <ntynen@gmail.com>
2022-11-17 15:44:07 -0800

    updated example data file with changes including; splitting out Roku hosts and ports, adding end times for primary and secondary TVs, adding the interact_delay, test_mode, and test_delay

--- data.py
+++ data.py
@@ -1,12 +1,18 @@
 data = {
-    'device_urls' : ['http://<your device IP>:8060/', 'http://<your device IP>:8060/'],
-    'shows' : ['Good Witch', 'Star Trek: Picard', 'Hallmark Movies & Mysteries'],
-    'channels' : ['Netflix', 'Paramount+', 'FrndlyTV'],
+    'device_hosts': ['192.168.86.38', '192.168.86.42'],
+    'service_port': '8060',  # The default port for Roku devices
+    'shows': ['Good Witch', 'Star Trek: Picard', 'Hallmark Movies & Mysteries'],
+    'channels': ['Netflix', 'Paramount+', 'FrndlyTV'],
     'channel_numbers': ['12', '31440', '298229'],
-    'frndly_guide_position' : 4,
-    'netflix_search_int' : 5,
-    'primary_tv_start_time' : [6, 29],
-    'secondary_tv_start_time' : [19, 29],
-    'update_delay' : 900,
-    'reset_tv_states' : [2, 00]
+    'frndly_guide_position': 4,
+    'netflix_search_int': 5,
+    'paramount_search_int': 6,
+    'primary_tv_start_time': [6, 29],
+    'primary_tv_end_time': [21, 00],
+    'secondary_tv_start_time': [19, 29],
+    'secondary_tv_end_time': [22, 00],
+    'update_delay': 3600,  # Each hour perform general housekeeping
+    'interact_delay': 1200,  # Every 20 minutes perform time specific tasks
+    'test_mode': False,  # This is only used for testing purposes - it should normally be FALSE
+    'test_delay': 600  # How long to wait between test cycles
 }
