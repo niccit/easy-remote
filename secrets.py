41a92ed276db771233e447511dcebb1f460c83f1
Nicci Tynen <ntynen@gmail.com>
2022-11-21 18:02:19 -0800

    Example secrets file with link to documentation Added link to documentation

---
+++ secrets.py
@@ -0,0 +1,7 @@
+secrets = {  # Full documentation https://learn.adafruit.com/adafruit-matrixportal-m4/internet-connect
+    'ssid': '<your ssid here>',
+    'password': '<password for your network>',
+    'timezone': '<example: America/New_York',  # See http://worldtimeapi.org/timezones
+    'aio_username': '<your Adafruit IO username>',  # See https://io.adafruit.com, info under the key once your account is set up
+    'aio_key': '<your Adafruit IO key>'
+}
